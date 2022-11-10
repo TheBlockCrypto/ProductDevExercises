a05a7086a3164aca35c51c70b545221a0e585125
Umar Sayyad <umarsayyad0@gmail.com>
2022-11-10 14:46:26 -0500

    added components to project, initiated storybook

---
+++ src/components/Todo/TodoContainer.js
@@ -0,0 +1,30 @@
+import "./Todo.scss";
+import DisplayTodos from "./DisplayTodos";
+import Todos from "./Todo";
+
+import { motion } from "framer-motion";
+
+function TodoContainer() {
+  return (
+    <div className="App">
+      <motion.h1
+        initial={{ y: -200 }}
+        animate={{ y: 0 }}
+        transition={{ type: "spring", duration: 0.5 }}
+        whileHover={{ scale: 1.1 }}
+      >
+        Todo App
+      </motion.h1>
+      <motion.div
+        initial={{ y: 1000 }}
+        animate={{ y: 0 }}
+        transition={{ type: "spring", duration: 1 }}
+      >
+        <Todos />
+        <DisplayTodos />
+      </motion.div>
+    </div>
+  );
+}
+
+export default TodoContainer;
